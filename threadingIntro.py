59e766a42531f99e09b5caa95a9b6281e4562da6
okeno1 <petercoin.1997@gmail.com>
2024-05-25 16:03:39 +0300

    Using map instead of list comprehension

--- threadingIntro.py
+++ threadingIntro.py
@@ -11,10 +11,17 @@ def do_something(seconds):
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     secs = [5,4,3,2,1]
+    '''
     results = [executor.submit(do_something,sec) for sec in secs]
     
     for f in concurrent.futures.as_completed(results):
         print(f.result())
+    '''
+    #Using map instead of list comprehension
+    results = executor.map(do_something, secs)
+    
+    for result in results:
+        print(result)
 
 '''
 threads = []
