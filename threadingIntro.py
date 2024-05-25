c759e7960830e3b3dac7eba88344e39a5029bc0d
okeno1 <petercoin.1997@gmail.com>
2024-05-25 15:01:36 +0300

    running fn 10 times with threads

--- threadingIntro.py
+++ threadingIntro.py
@@ -7,17 +7,16 @@ def do_something():
     print('Sleeping 1 second...')
     time.sleep(1)
     print('Done Sleeping')
+
+threads = []
     
-#do_something()
-#do_something()
-t1 = threading.Thread(target=do_something)
-t2 = threading.Thread(target=do_something)
-
-t1.start()
-t2.start()
-
-t1.join()
-t2.join()
+for _ in range(10):
+    t = t1 = threading.Thread(target=do_something)
+    t.start()
+    threads.append(t)
+    
+for thread in threads:
+    thread.join()
 
 finish = time.perf_counter()
 
