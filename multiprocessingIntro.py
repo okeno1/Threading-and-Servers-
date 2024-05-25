e8111f01de865456b4a9b03eb5d0571d3aaa21ac
okeno1 <petercoin.1997@gmail.com>
2024-05-26 00:25:26 +0300

    SingleThread Multiprocessing using ProcessPoolExecutor

--- multiprocessingIntro.py
+++ multiprocessingIntro.py
@@ -30,29 +30,35 @@ print(f'Finished in {round(finish-start,2)} second(s)')
 '''
 ###############################################################
 #######################MultiProcessing#########################
-import multiprocessing
+#import multiprocessing
+import concurrent.futures
 import time
 
 def do_something(seconds):
     print(f"Sleeping {seconds} second(s)...")
     time.sleep(seconds)
-    print('Done Sleeping...')
+    return 'Done Sleeping...'
 
 if __name__ == "__main__":
     start = time.perf_counter()
 
-    processes=[]
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        f1 = executor.submit(do_something, 1)
+        print(f1.result())
+
+    # Uncomment the block below to use multiprocessing manually
+    '''
+    processes = []
     
     for _ in range(10):
-        p = multiprocessing.Process(target=do_something,args=[1.5])
+        p = multiprocessing.Process(target=do_something, args=[1.5])
         p.start()
         processes.append(p)
         
     for process in processes:
         process.join()
-    
+    '''
 
     finish = time.perf_counter()
 
-    print(f'Finished in {round(finish-start,2)} second(s)')
-
+    print(f'Finished in {round(finish-start, 2)} second(s)')
